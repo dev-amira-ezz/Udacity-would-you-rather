6e308e731d5981031dc6a53045e69dc459ddccfd
Amira Ezzeldin <aezz4007@gmail.com>
2021-10-16 12:35:17 +0200

    feat: created store, state and middleware

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import handleInitialData from '../actions/shared'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
       <div className="App">
@@ -14,4 +19,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
